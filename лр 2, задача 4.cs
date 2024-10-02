c67cc02364193aafae90677033bae340b870f29b
Milwayky <vyryshevaalisa@gmail.com>
2024-10-02 21:29:53 +0600

    Add files via upload

---
+++ лр 2, задача 4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int counter5 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (Math.Abs(a) % 10 == 5)
+            {
+            counter5 += 1;
+            }
+        
+        }
+        Console.WriteLine($"Количество элементов, оканчивающихся на 5: {counter5}");
+    }
+}
